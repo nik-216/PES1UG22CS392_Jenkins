5cadd704babbf0622818184bfaaef429822f3200
Nikitha <nik-216@users.noreply.github.com>
2025-03-18 16:15:11 +0530

    changed main 2

--- main/hello.cpp
+++ main/hello.cpp
@@ -5,7 +5,8 @@ using namespace std;
 int main(void) {
   cout << "Hello, World\n";
   cout << "Hello, Jenkins\n";
-  cout << "Hello PES1UG22CS392";
+  cout << "Hello PES1UG22CS392\n";
+  cout << "Hello Nikitha";
   cout << "I have successfully built and run this  \n";
   return 0;
 }
