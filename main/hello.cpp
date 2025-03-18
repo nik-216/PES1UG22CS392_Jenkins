754a058a1995d6b0c7a26b0898d1f74e6353276d
Nikitha <nik-216@users.noreply.github.com>
2025-03-18 16:18:34 +0530

    changed main 2

--- main/hello.cpp
+++ main/hello.cpp
@@ -6,7 +6,7 @@ int main(void) {
   cout << "Hello, World\n";
   cout << "Hello, Jenkins\n";
   cout << "Hello PES1UG22CS392\n";
-  cout << "Hello Nikitha";
+  cout << "Hello\n";
   cout << "I have successfully built and run this  \n";
   return 0;
 }
